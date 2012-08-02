26dbb99d3c1d394e43090f01df9f9907771f9e4f
Osada <osada@hms>
2012-08-02 13:29:52 -0700

    displaying a form is working

--- NP_ContactForm.php
+++ NP_ContactForm.php
@@ -113,6 +113,93 @@ class NP_ContactForm extends NucleusPlugin
   		return $variable;
 	}
 
+	
+
+	// Show form
+	// Generate HTML code for a contact form based on custom markups entered by a user. 
+	// Return true is succeed.
+	//
+	function showForm($markup, $itemid)
+	{
+		// Store markup information in an array
+		$settings = array();
+		$a_keys = array('type', 'name', 'required', 'option');
+		preg_match_all('/\[%\(.*%\]/', $markup, $matches);
+		foreach ($matches as $match) {
+			$pattern = array('/\[%\(/', '/\)%\]/');
+			$stripped = preg_replace($pattern, '', $match);
+			foreach ( $stripped as $set) {
+				$a_value = explode(',',$set);
+				$i = 0;
+				foreach ( $a_keys as $key ) {
+					$setting[$key] = $a_value[$i];
+					$i++;
+				}
+				array_push($settings, $setting);	
+			}
+		}
+
+		// Create HTML tag based on markup information
+		// Tags generated are stored in an array along with the markup inforamtion. 
+		$i = 0;
+		foreach ($settings as $setting) {
+			if ($setting['type'] == 'text') {
+				$tag = '<input type="text" name="' . $setting['name'] . '" id="' . $setting['name'];
+				if (isset($setting['option'])) {
+					$tag .= '" value = "' . $setting['option'];
+				}
+				$tag .= '" />';
+			} elseif ($setting['type'] == 'checkbox' || $setting['type'] == 'radio') {
+				$options = explode('|', $setting['option']);
+				$optid = 1;
+				$tag = '';
+				foreach ($options as $option) {
+					$tag .= '<input type="' . $setting['type'] . '" ' 
+						 . 'name="' . $setting['name'] . '" '
+						 . 'id="' . $setting['name'] . $optid . '" ' 
+						 . 'value="' . $option . '" />' . "\r\n";
+					$optid++;
+				}
+			} elseif ($setting['type'] == 'select') {
+				$tag = '<select name="' . $setting['name'] . '" id="' . $setting['name'] . '">' . "\r\n";
+				preg_match_all('/[a-zA-Z0-9]+\|+?[a-zA-Z0-9]+/', $setting['option'], $matches);
+				foreach ($matches[0] as $match) {
+					$option = explode('|', $match);
+					$option = explode('|', $match);
+					$tag .= '<option value="' . $option[1] . '">' . $option[0] . '</option>' . "\r\n";
+				}
+				$tag .= '</select>' . "\r\n";
+			} elseif ($setting['type'] == 'textarea') {
+				$tag = '<textarea name="' . $setting['name'] . '" id="' . $setting['name'] .'">' . $setting['option'] . '</textarea>';
+			} elseif ($setting['type'] == 'submit') {
+				$tag = '<input type="submit" name="submit" id="submit" value="' . $setting['name'] . '" />' . "\r\n";
+			} else {
+				$tag = '';
+			}
+			$setting['tag'] = $tag;
+			$settings[$i] = $setting;
+			$i++;
+		}
+
+		// Replace custom markups with HTML tags
+		// Add <form> tag to finished product and generate a code for a form.
+		//preg_match_all('/\[%\(.*\)%\]/', $markup, $matches);
+		preg_match_all('/\[%\((\w+),(\w+).*\)%\]/', $markup, $matches);
+		$tags = array();
+		foreach ($settings as $setting) {
+			array_push($tags, $setting['tag']);
+		}
+		$markup = str_replace($matches[0], $tags, $markup);
+
+		$code = '<form name="contactform" id="contactform" method="post" action="'
+		      . createItemLink($itemid) . '" onsubmit="return showProcess();">' . "\r\n";
+		$code .= $markup;
+		$code .= '</form>' . "\r\n"
+			  .  '<div id="other" class="hidePane">Processing</div>' . "\r\n";
+		
+		echo $code . $this->addJs() . $this->addCss() . "\r\n";
+	}
+	
 	// returns JavaScript for displaying 'processing...' message
 	function addJs()
 	{
@@ -160,160 +247,6 @@ EOT;
 			</style>
 EOT;
 		return $css;
-	}
-
-	// Show form
-	// Generate HTML code for a contact form based on custom markups entered by a user. 
-	// Return true is succeed.
-	//
-	function showForm($markup, $itemid)
-	{
-		// Store markup information in an array
-		$settings = array();
-		$a_keys = array('type', 'name', 'required', 'option');
-		preg_match_all('/\[%\(.*%\]/', $markup, $matches);
-		foreach ($matches as $match) {
-			$pattern = array('/\[%\(/', '/\)%\]/');
-			$stripped = preg_replace($pattern, '', $match);
-			foreach ( $stripped as $set) {
-				$a_value = explode(',',$set);
-				$i = 0;
-				foreach ( $a_keys as $key ) {
-					$setting[$key] = $a_value[$i];
-					$i++;
-				}
-				array_push($settings, $setting);	
-			}
-		}
-
-		// Create HTML tag based on markup information
-		$i = 0;
-		foreach ($settings as $setting) {
-			if ($setting['type'] == 'text') {
-				$tag = '<input type="text" name="' . $setting['name'] . '" id="' . $setting['name'];
-				if (isset($setting['option'])) {
-					$tag .= '" value = "' . $setting['option'];
-				}
-				$tag .= '" />';
-			} elseif ($setting['type'] == 'checkbox' || $setting['type'] == 'radio') {
-				$options = explode('|', $setting['option']);
-				$optid = 1;
-				foreach ($options as $option) {
-					$tag .= '<input type="checkbox" ' 
-						 . 'name="' . $setting['name'] . '" '
-						 . 'id="' . $setting['name'] . $optid . '" ' 
-						 . 'value="' . $option . '" />' . "\r\n";
-					$optid++;
-				}
-			} elseif ($setting['type'] == 'select') {
-				$tag = '<select name="' . $setting['name'] . '" id="' . $setting['name'] . '">' . "\r\n";
-				preg_match_all('/[a-zA-Z0-9]+\|+?[a-zA-Z0-9]+/', $setting['option'], $matches);
-				foreach ($matches[0] as $match) {
-					$option = explode('|', $match);
-					$option = explode('|', $match);
-					$tag .= '<option value="' . $option[1] . '">' . $option[0] . '</option>' . "\r\n";
-				}
-				$tag .= '</select>' . "\r\n";
-			} elseif ($setting['type'] == 'textarea') {
-				$tag = '<textarea name="' . $setting['name'] . '" id="' . $setting['name'] .'">' . $setting['option'] . '</textarea>';
-			} elseif ($setting['type'] == 'submit') {
-				$tag = '<input type="submit" name="submit" id="submit" value="' . $setting['name'] . '" />"' . "\r\n";
-			} else {
-				$tag = '';
-			}
-			$setting['tag'] = $tag;
-			$settings[$i] = $setting;
-			$i++;
-		}
-		print_r($settings);
-	}
-
-
-
-
-	/*
-	// Show form
-	// Use user code for a form, if a user has set it through plugin option page,
-	// else use default code for a form, a code which defined in addForm().
-	function showForm($itemid) 
-	{
-		$form = '<form name="contactform" id="contactform" method="post" action="' 
-			  . createItemLink($itemid) . '" onsubmit="return showProcess();">' ."\r\n";
-		$userform = trim($this->getOption("form"));
-		if ($userform != '') {
-			$form .= $userform;
-			$postvar = implode(',', $this->grabNameAttr($userform));
-			$form .= '<input type="hidden" value="' . $postvar . '" name="postvar" />';
-			$required = implode(',', $this->grabRequired($userform, $postvar));
-			$form .= '<input type="hidden" value="' . $required . '" name="required" />';			
-		} else {
-			$form .= $this->addForm();
-		}
-		$form .= '</form>' . "\r\n"
-			   . '<div id="other" class="hidePane">Processing...</div>' . "\r\n";		
-		
-		echo $form . $this->addJs() . $this->addCss() . "\r\n";
-	}
-	 */
-	
-	/*
-	// function to validate form code entered by a user
-	// user HTMLPurifier in future for more advanced filtering
-	// It takes a code entred by a user and returns error message. (empty if no error)
-	function validate($userform)
-	{	
-		$errormsg = '';
-		// match form elements
-		preg_match_all('/(<)((input|textarea|select|button)).*?(>)/', $userform, $matches);
-		$numfield = count($matches[0]); // number of allowed form elements
-		
-		// match name attribute
-		preg_match_all('/(name=("|\').*?("|\'))/', $userform, $matches);
-		$numname = count($matches[0]); // number of name attribute
-		if ($numfield != $numname ) {
-			$errormsg .=  
-				'Please make sure to add name attribute to each form element.<br />' . "\r\n";
-		}
-		// check if code include <form>
-		preg_match_all('/(<)(form).*?(>)|(<\/form>)/', $userform, $matches);
-		if (count($matches[0])>0){
-			$errormsg .= 'Do not include &lt;form&gt; tag.<br />' . "\r\n"; 
-		}
-		// make error message colorful
-		if (strlen($errormsg) > 0 ){
-			$errormsg = '<div style="color:#FF0000;">' . $errormsg;
-			$errormsg .= '</div>';
-		}
-		return $errormsg;
-	}
-	*/
-
-	// Grab name attributes from a code entered by a user.
-	// Returns an array of attributes to be used in $_POST. 
-	function grabNameAttr($code)
-	{
-		preg_match_all('/(name=("|\').*?("|\'))/', $code, $matches);
-
-		$attributes = array();
-		$pattern = array('/("|\')/','/name=/');		
-		
-		foreach ($matches[0] as $match){
-			$attr = preg_replace($pattern, '', $match);
-			if ($attr != 'submit') {
-				array_push($attributes, $attr);
-			}
-		}
-		return $attributes;
-	}
-
-	// Grab form element with class name 'required'.
-	// Returns an array of names to be used in $_POST. 
-	function grabRequired($code,$postvar)
-	{
-		preg_match_all('/(<).*(class=("|\')required("|\')).*(>)/', $code, $matches);
-		$tags = implode(" ", $matches[0]);	
-		$required = $this->grabNameAttr($tags);
-		return $required;
 	}
 	
 	// Validate email
